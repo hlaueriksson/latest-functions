c426addd54e54b299b14eae7872c7a5d1ea9c170
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2018-09-27 19:28:58 +0200

    :package: Update CommandQuery.AzureFunctions

--- src/LatestFunctions/LatestFunctions/Query.cs
+++ src/LatestFunctions/LatestFunctions/Query.cs
@@ -1,6 +1,6 @@
 ﻿using System.Threading.Tasks;
-using Autofac;
 using CommandQuery.AzureFunctions;
+using CommandQuery.DependencyInjection;
 using LatestFunctions.Queries;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -8,12 +8,13 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace LatestFunctions
 {
     public static class Query
     {
-        private static readonly QueryFunction Func = new QueryFunction(typeof(Query).Assembly.GetQueryProcessor(GetContainerBuilder()));
+        private static readonly QueryFunction Func = new QueryFunction(typeof(Query).Assembly.GetQueryProcessor(GetServiceCollection()));
 
         [FunctionName("Query")]
         public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "query/{queryName}")] HttpRequest req, TraceWriter log, string queryName)
@@ -21,27 +22,24 @@ namespace LatestFunctions
             return await Func.Handle(queryName, req, log);
         }
 
-        private static ContainerBuilder GetContainerBuilder()
+        private static IServiceCollection GetServiceCollection()
         {
             var config = new ConfigurationBuilder()
                 .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
                 .AddEnvironmentVariables()
                 .Build();
 
-            var builder = new ContainerBuilder();
-            builder
-                .RegisterInstance(
-                    new Configuration
-                    {
-                        BlogQueryHandlerFeedUri = config["BlogQueryHandlerFeedUri"],
-                        GitHubQueryHandlerUsername = config["GitHubQueryHandlerUsername"],
-                        InstagramQueryHandlerUsername = config["InstagramQueryHandlerUsername"]
-                    })
-                .As<IBlogQueryHandlerConfiguration>()
-                .As<IGitHubQueryHandlerConfiguration>()
-                .As<IInstagramQueryHandlerConfiguration>();
+            var configuration = new Configuration
+            {
+                BlogQueryHandlerFeedUri = config["BlogQueryHandlerFeedUri"],
+                GitHubQueryHandlerUsername = config["GitHubQueryHandlerUsername"],
+                InstagramQueryHandlerUsername = config["InstagramQueryHandlerUsername"]
+            };
 
-            return builder;
+            return new ServiceCollection()
+                .AddSingleton<IBlogQueryHandlerConfiguration>(_ => configuration)
+                .AddSingleton<IGitHubQueryHandlerConfiguration>(_ => configuration)
+                .AddSingleton<IInstagramQueryHandlerConfiguration>(_ => configuration);
         }
     }
 }
